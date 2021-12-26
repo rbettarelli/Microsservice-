d2d6014de469ee35f00b9844add5d911edacc603
Ricardo Bettarelli <ricardo.bettarelli@gmail.com>
2021-12-26 18:44:13 -0300

    Criando repository e Service para categoria

---
+++ src/main/java/com/ricardo/coursemc/service/CategoriaService.java
@@ -0,0 +1,24 @@
+package com.ricardo.coursemc.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ricardo.coursemc.domain.Categoria;
+import com.ricardo.coursemc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	
+	}
+
+}
