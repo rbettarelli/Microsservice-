b8ddb0ea9b0fc727b182787b20a79ae69a113012
Ricardo Bettarelli <ricardo.bettarelli@gmail.com>
2021-12-26 10:43:03 -0300

    Projeto Criado

---
+++ src/main/java/com/ricardo/coursemc/CouserMcApplication.java
@@ -0,0 +1,13 @@
+package com.ricardo.coursemc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouserMcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouserMcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ricardo/coursemc/CouserMcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricardo.coursemc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouserMcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
