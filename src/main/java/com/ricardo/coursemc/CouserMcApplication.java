fd6a8a96e7f8dfc01734f55fca27b60df020a5b8
Ricardo Bettarelli <ricardo.bettarelli@gmail.com>
2021-12-26 19:24:15 -0300

    Criando Operacao de Instanciação

--- src/main/java/com/ricardo/coursemc/CouserMcApplication.java
+++ src/main/java/com/ricardo/coursemc/CouserMcApplication.java
@@ -1,13 +1,35 @@
 package com.ricardo.coursemc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CouserMcApplication {
+import com.ricardo.coursemc.domain.Categoria;
+import com.ricardo.coursemc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CouserMcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CouserMcApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+	}
+	
+	
 
 }
