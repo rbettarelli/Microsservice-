554ee84c7c10f3834c09df6ef2e930e3912fadd3
Ricardo Bettarelli <ricardo.bettarelli@gmail.com>
2021-12-28 10:47:57 -0300

    Atualizando Categoria com Put

--- src/main/java/com/ricardo/coursemc/service/CategoriaService.java
+++ src/main/java/com/ricardo/coursemc/service/CategoriaService.java
@@ -11,23 +11,23 @@ import com.ricardo.coursemc.service.exception.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
-	
+
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
-		"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
-		}
-	
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
 	}
-	
+
+	public Categoria update(Categoria obj) {
+		return repo.save(obj);
 	}
-	
 
-
+}
