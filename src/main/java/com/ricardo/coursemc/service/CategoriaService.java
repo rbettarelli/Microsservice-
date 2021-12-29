babb0a869af7542d3825492525095d24fcd4c3ec
Ricardo Bettarelli <ricardo.bettarelli@gmail.com>
2021-12-29 00:03:39 -0300

    Valicadao Sintatica com Bean Valitation

--- src/main/java/com/ricardo/coursemc/service/CategoriaService.java
+++ src/main/java/com/ricardo/coursemc/service/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.ricardo.coursemc.domain.Categoria;
+import com.ricardo.coursemc.dto.CategoriaDTO;
 import com.ricardo.coursemc.repositories.CategoriaRepository;
 import com.ricardo.coursemc.service.exception.DataIntegrityException;
 import com.ricardo.coursemc.service.exception.ObjectNotFoundException;
@@ -54,6 +55,11 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+		
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 		
 	}
 
