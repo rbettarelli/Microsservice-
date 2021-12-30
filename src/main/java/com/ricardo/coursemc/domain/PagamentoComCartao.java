16cffd7f90a7f209ae644837fc4988f8a418838c
Ricardo Bettarelli <ricardo.bettarelli@gmail.com>
2021-12-30 19:42:08 -0300

    Inserindo Pedido

--- src/main/java/com/ricardo/coursemc/domain/PagamentoComCartao.java
+++ src/main/java/com/ricardo/coursemc/domain/PagamentoComCartao.java
@@ -2,10 +2,11 @@ package com.ricardo.coursemc.domain;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.ricardo.coursemc.domain.enums.EstadoPagamento;
 
 @Entity
-
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento {
 	private static final long serialVersionUID = 1L;
 
--- src/main/java/com/ricardo/coursemc/resources/PedidoResource.java
+++ src/main/java/com/ricardo/coursemc/resources/PedidoResource.java
@@ -1,15 +1,23 @@
 package com.ricardo.coursemc.resources;
 
+import java.net.URI;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.ricardo.coursemc.domain.Categoria;
 import com.ricardo.coursemc.domain.Pedido;
+import com.ricardo.coursemc.dto.CategoriaDTO;
 import com.ricardo.coursemc.service.PedidoService;
-	
+
 @RestController
 @RequestMapping(value = "/pedidos")
 public class PedidoResource {
@@ -23,7 +31,12 @@ public class PedidoResource {
 		return ResponseEntity.ok().body(obj);
 
 	}
-	}		
 
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody Pedido obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 
-
+	}
+}
