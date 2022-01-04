ad309e82ca0a96cf66598d490653b06dafa34f79
Ricardo Bettarelli <ricardo.bettarelli@gmail.com>
2022-01-04 20:03:51 -0300

    Token Refresh

---
+++ src/main/java/com/ricardo/coursemc/resources/AuthResource.java
@@ -0,0 +1,30 @@
+package com.ricardo.coursemc.resources;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ricardo.coursemc.security.JwtUtil;
+import com.ricardo.coursemc.security.UserSS;
+import com.ricardo.coursemc.service.UserService;
+
+@RestController
+@RequestMapping(value = "/auth")
+public class AuthResource {
+	
+	@Autowired
+	private JwtUtil jwtUtil;
+
+	@RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
+	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+		UserSS user = UserService.authenticated();
+		String token = jwtUtil.generateToken(user.getUsername());
+		response.addHeader("Authorization", "Bearer " + token);
+		return ResponseEntity.noContent().build();
+	}
+
+}
