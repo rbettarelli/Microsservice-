9815eb641bc71efe84b8123582a4a9a9577fcc2a
Ricardo Bettarelli <ricardo.bettarelli@gmail.com>
2022-01-04 23:37:33 -0300

    COnfigurando S3

--- src/main/java/com/ricardo/coursemc/CouserMcApplication.java
+++ src/main/java/com/ricardo/coursemc/CouserMcApplication.java
@@ -1,12 +1,20 @@
 package com.ricardo.coursemc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.ricardo.coursemc.service.S3Service;
+
 @SpringBootApplication
 public class CouserMcApplication implements CommandLineRunner {
 
+	
+	@Autowired
+	private S3Service s3Service;
+	
+	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CouserMcApplication.class, args);
@@ -14,6 +22,9 @@ public class CouserMcApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		s3Service.uploadFile("C:\\Users\\ricar\\OneDrive\\Documentos\\Udemy\\Springionic\\rick.jpg");
+		
+		
 
 		
 	}
